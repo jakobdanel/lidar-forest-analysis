e385aa338c7a0b9c16990cfebfaf6cd77db33caf
Jakob <jakobdanel@aol.com>
2023-11-07 15:49:02 +0100

    Add the library call and an return

--- R/download.R
+++ R/download.R
@@ -1,4 +1,7 @@
+
 download <- function(species, name, location) {
+  library(GEDIcalibratoR)
   mytiles = intersect_tiles2download("NRW", location) |>
     download_tiles(dir = file.path("..", "data", species, name), what = "LAZ")
+  return(mytiles)
 }
