acad7a1247212cdaedd5f909af6e4f7a7cc709e1
Jakob <jakobdanel@aol.com>
2023-11-07 15:50:04 +0100

    Implemented retiling
    - If the file is not exisitent stop
    - If the retiles are already computed write an warning
    - Compute the retiles if necessary write them into an file

---
+++ R/retile.R
@@ -0,0 +1,25 @@
+
+
+
+retile <- function(species, name, tile_name) {
+  retile_dir = file.path("..", "data", species, name, tile_name)
+  if (file.exists(retile_dir) && file.info(retile_dir)$isdir) {
+    warning("Area is already retiled, no further computings")
+  } else {
+    if (!file.exists(file.path("..", "data", species, name, paste0(tile_name, ".laz")))) {
+      stop("File do not exist!")
+    } else {
+      library(lidR)
+      library(future)
+      catalog = readLAScatalog(file.path("..", "data", species, name, paste0(tile_name, ".laz")))
+      plan(multisession(workers = 6))
+      opt_chunk_size(catalog) <- 50
+      opt_chunk_buffer(catalog) <- 0
+      output_location <-
+        paste0(getwd(), "/", retile_dir, "/tile_{ID}")
+      cat("Write files to: ", output_location)
+      opt_output_files(catalog) <- output_location
+      return(catalog_retile(catalog))
+    }
+  }
+}
