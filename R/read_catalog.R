d4ad280b5be1e881b1b90c673c0cb36236d06992
Jakob <jakobdanel@aol.com>
2023-11-13 15:04:43 +0100

    Modify files for usage in R package
    - Notation that the objects will be exported
    - Remove sourcing of dependencies

--- R/read_catalog.R
+++ R/read_catalog.R
@@ -1,15 +1,8 @@
-source("./R/utils/files.R")
-source("./R/tile_location.R")
-
+#' @export
 read_catalog <- function(tile_location) {
   library(lidR)
-  return(
-    get_retile_dir(
-      tile_location@species,
-      tile_location@name,
-      tile_location@tile_name
-    ) |>
-      list.files(full.names = T) |>
-      readLAScatalog()
-  )
+  return(tile_location |>
+           get_retile_dir() |>
+           list.files(full.names = T) |>
+           readLAScatalog())
 }
