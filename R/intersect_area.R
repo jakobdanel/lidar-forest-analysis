a448da0102720dda4d317e57919aaf1d167c6426
Jakob <jakobdanel@aol.com>
2023-12-04 19:02:14 +0100

    feat(#15): Fixed bug by running the function manually

--- R/intersect_area.R
+++ R/intersect_area.R
@@ -70,3 +70,65 @@ lfa_intersect_areas <- function(ctg, tile_location, areas_sf) {
   return(read_catalog(tile_location))
 
 }
+
+#'@export
+lfa_intersect_areas_man <- function(ctg, tile_location, areas_sf) {
+  #Find the correct polygon
+  index_correct_area = -1
+  for (i in 1:length(areas_sf$species)) {
+    specie <- areas_sf$species[i]
+    name <- areas_sf$name[i]
+    if (tile_location@species == specie &&
+        tile_location@name == name) {
+      index_correct_area = i
+    }
+  }
+  if (index_correct_area == -1) {
+    stop("No valid area was found in the data.")
+  }
+  area <- areas_sf[index_correct_area,]
+
+  ctg <- lfa_load_ctg_if_not_present(ctg, tile_location)
+  area <- sf::st_transform(area, st_crs(ctg))
+  identify_area <- function(chunk) {
+    if (is.empty(chunk)) {
+      return(NULL)
+    } else {
+      tryCatch(
+        {
+          # Merge spatial information
+          chunk <- merge_spatial(chunk, area, attribute = "isInside")
+
+          # Filter out points that are not classified as outside
+          chunk <- chunk[chunk$isInside, ]
+
+          # Check if there are points remaining after filtering
+          if (is.empty(chunk) || length(chunk$X) == 0 || nrow(chunk) == 0) {
+            warning("No points found after filtering. Returning NULL.")
+            return(NULL)
+          }
+
+          return(chunk)
+        },
+        error = function(e) {
+          warning("Error in merge_spatial: ", conditionMessage(e))
+          return(NULL)
+        }
+      )
+    }
+  }
+
+  for(f in ctg$filename){
+    las = lidR::readLAS(f)
+    res = identify_area(las)
+    file.remove(f)
+    if(is.null(res)){
+      cat("No intersecting area found for ", f)
+    }else {
+      lidR::writeLAS(res,f)
+    }
+  }
+
+  return(read_catalog(tile_location))
+}
+
