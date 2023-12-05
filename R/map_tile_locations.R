d6517f9b035aebd36a6beac1ed45b1a397249aa2
Jakob <jakobdanel@aol.com>
2023-12-05 16:10:02 +0100

    Implement a function to iterate over all tile locations

---
+++ R/map_tile_locations.R
@@ -0,0 +1,50 @@
+#' Map Function Over Tile Locations
+#'
+#' This function applies a specified mapping function to each tile location in a list.
+#'
+#' @param tile_locations A list of tile location objects.
+#' @param map_function The mapping function to be applied to each tile location.
+#'   The function should take a tile location object as its first argument,
+#'   and additional arguments can be passed using the ellipsis (`...`) syntax.
+#' @param ... Additional arguments to be passed to the mapping function.
+#'
+#' @return None
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Example usage
+#' lfa_map_tile_locations(tile_locations, my_mapping_function, param1 = "value")
+#' }
+#'
+#' @param tile_locations A list of tile location objects.
+#' @param map_function The mapping function to be applied to each tile location.
+#' @param ... Additional arguments to be passed to the mapping function.
+#'
+#' @details
+#' This function iterates over each tile location in the provided list (`tile_locations`)
+#' and applies the specified mapping function (`map_function`) to each tile location.
+#' The mapping function should accept a tile location object as its first argument, and
+#' additional arguments can be passed using the ellipsis (`...`) syntax.
+#'
+#' This function is useful for performing operations on multiple tile locations concurrently,
+#' such as loading Lidar data, processing areas, or other tasks that involve tile locations.
+#'
+#' @seealso
+#' The mapping function provided should be compatible with the structure and requirements
+#' of the tile locations and the specific task being performed.
+#'
+#' @examples
+#' \dontrun{
+#' # Example usage
+#' lfa_map_tile_locations(tile_locations, my_mapping_function, param1 = "value")
+#' }
+#'
+#' @export
+lfa_map_tile_locations <-
+  function(tile_locations, map_function, ...) {
+    for (i in 1:length(tile_locations)) {
+      map_function(tile_location = tile_locations[[i]], ...)
+    }
+  }
