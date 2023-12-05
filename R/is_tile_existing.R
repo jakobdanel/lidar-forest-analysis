a0e81a07c62a1a657c8c92f78c0f76f2ac60c192
Jakob <jakobdanel@aol.com>
2023-12-05 10:19:50 +0100

    Built site for gh-pages

--- R/is_tile_existing.R
+++
@@ -1,4 +0,0 @@
-#' @export
-is_tile_existing <- function(tile_location) {
-  return(get_tiles_path(tile_location) |> file.exists())
-}
--- R/read_catalog.R
+++
@@ -1,8 +0,0 @@
-#' @export
-read_catalog <- function(tile_location) {
-  library(lidR)
-  return(tile_location |>
-           get_retile_dir() |>
-           list.files(full.names = T) |>
-           readLAScatalog())
-}
