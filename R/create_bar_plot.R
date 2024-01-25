6ea0961b7a33c9df475fbd3e0e3000c7d05b2c19
Jakob <jakobdanel@aol.com>
2024-01-25 13:38:26 +0100

    Add additional parametr to create bar plot function

--- R/create_bar_plot.R
+++ R/create_bar_plot.R
@@ -8,6 +8,10 @@
 #' @param value_column The column containing the values to be plotted.
 #' @param label_column The column used for labeling the bars on the x-axis. Default is "name".
 #' @param grouping_column The column used for grouping the bars. Default is "species".
+#' @param xlab Label for x-Axis.
+#' @param ylab Label for y-Axis.
+#' @param title Title of the plot.
+#'
 #' @return A ggplot2 barplot.
 #'
 #'
@@ -16,23 +20,41 @@
 #' lfa_create_barplot(your_data_frame, value_column = "value", label_column = "name", grouping_column = "species")
 #'
 #'@export
-lfa_create_grouped_bar_plot <- function(data, grouping_var, value_col, label_col) {
+lfa_create_grouped_bar_plot <-
+  function(data,
+           grouping_var,
+           value_col,
+           label_col,
+           xlab = "Name of Patch",
+           ylab = "Density",
+           title = "Tree density across the different patches, grouped by specie") {
+    if (!(
+      grouping_var %in% colnames(data) &&
+      value_col %in% colnames(data) && label_col %in% colnames(data)
+    )) {
+      stop("Columns not found in the data.frame.")
+    }
 
-if (!(grouping_var %in% colnames(data) && value_col %in% colnames(data) && label_col %in% colnames(data))) {
-  stop("Columns not found in the data.frame.")
-}
+    # Create a grouped bar plot
+    plot <-
+      ggplot2::ggplot(data, ggplot2::aes(
+        x = reorder(data[[label_col]], data[[value_col]]),
+        y = data[[value_col]],
+        fill = data[[grouping_var]]
+      )) +
+      ggplot2::geom_bar(stat = "identity", position = "dodge") +
+      ggplot2::labs(
+        x = xlab,
+        y = ylab,
+        fill = "Specie",
+        title = title
+      ) +
+      ggplot2::theme_minimal() +
+      ggplot2::theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-# Create a grouped bar plot
-plot <- ggplot2::ggplot(data, ggplot2::aes(x = reorder(data[[label_col]], data[[value_col]]), y = data[[value_col]], fill = data[[grouping_var]])) +
-  ggplot2::geom_bar(stat = "identity", position = "dodge") +
-  ggplot2::labs(x = "Name of patch", y = "Density", fill = "Specie", title = "Tree density across the different patches, grouped by specie") +
-  ggplot2::theme_minimal() +
-  ggplot2::theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-return(plot)
-}
+    return(plot)
+  }
 
 # Example usage:
 # Assuming 'my_data' is your data.frame, 'category' is the grouping variable,
 # 'value' is the column with values, and 'label' is the column with labels.
-
